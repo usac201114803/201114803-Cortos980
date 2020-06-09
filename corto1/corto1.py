35e5d4da0cb1c3a519116116c9758d9ed4705fec
Ottoniel Sierra <ottonielsierrama@gmail.com>
2020-06-09 17:44:06 -0600

    corto calificado

---
+++ corto1/corto1.py
@@ -0,0 +1,8 @@
+#Funcionamiento:        0/40
+#Uso de Funciones       0/20
+#Manejo de archivos     0/10
+#Manejo de Listas       0/10
+#Uso de git             20/20
+#*****************************
+#Total                  /100
+
